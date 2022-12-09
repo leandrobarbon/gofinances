bc669a655d1623e825911cf2c95e631c0ab71529
Leandro da silva <leandrosilva.hair@gmail.com>
2022-12-08 22:30:25 -0300

    new feature

---
+++ src/components/HistoryCard/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styles';
+
+export function HistoryCard() {
+  return (
+    <Container color={color}>
+      <Title></Title>
+      <Amount></Amount>
+    </Container>
+  )
+}
---
+++ src/screens/Resume/index.tsx
@@ -0,0 +1,19 @@
+import { HistoryCard } from "../../components/HistoryCard";
+import {
+  Container,
+  Header,
+  Title,
+} from "./styles";
+
+export function Resume() {
+  return (
+    <Container>
+      <Header>
+        <Title>Resumo por categoria</Title>
+      </Header>
+
+      <HistoryCard />
+
+    </Container>
+  )
+}
